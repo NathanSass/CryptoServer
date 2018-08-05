5b10bf765c8c25f4a26fb0225ccc04422d33feb2
nathan <nathans@scribd.com>
2018-08-05 08:34:52 -0700

    added urls and rendering chapters

--- src/main/kotlin/routes.kt
+++ src/main/kotlin/routes.kt
@@ -15,7 +15,29 @@ fun main(args: Array<String>) {
         res.render("test.handlebars")
     })
 
+    app.get("/playlist/master", { req, res ->
+        res.setHeader("Content-Type", "application/x-mpegURL")
+        res.render("master.m3u.handlebars")
+    })
+
+    app.get("/playlist", { req, res ->
+        res.setHeader("Content-Type", "application/x-mpegURL")
+        res.render("m3u.handlebars", Factory.audiobook())
+    })
+
     app.listen(3000, {
         println("Listening on port 3000")
     })
 }
+
+object Factory {
+    fun audiobook() : Audiobook{
+        val chap1 = Chapter(1, 290, "https://ia800504.us.archive.org/24/items/Item548HowToGoProWithAnEntryLevelDSLR/OMHT548.mp3" )
+        val chap2 = Chapter(2, 215, "https://ia800500.us.archive.org/32/items/Item335HowToMeditate/OMHT335.mp3")
+        return Audiobook(290 + 215, arrayOf(chap1, chap2))
+    }
+}
+
+data class Audiobook(val duration: Long, val chapters: Array<Chapter>)
+
+data class Chapter(val index: Int, val duration: Long, val url: String)
