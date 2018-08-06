c3e4dde3e0b699fc6b5199a00c1a222c266fedb0
nathan <nathans@scribd.com>
2018-08-06 09:55:05 -0700

    streams content YAY

---
+++ js/jshelpers.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.toHexString = function(byteArray) {
+    return byteArray.toString('hex')
+}
--- src/main/kotlin/routes.kt
+++ src/main/kotlin/routes.kt
@@ -1,5 +1,8 @@
 external fun require(module:String):dynamic
 
+val crypto = require("crypto")
+val jsHelpers = require("../js/jshelpers")
+
 fun main(args: Array<String>) {
     println("Hello JavaScript!")
 
@@ -7,6 +10,7 @@ fun main(args: Array<String>) {
     val app = express()
     val expressHandlebars = require("express-handlebars")
     val hbs = expressHandlebars.create()
+    val request = require("request")
     app.engine("handlebars", hbs.engine)
     app.set("view engine', 'handlebars")
 
@@ -22,7 +26,24 @@ fun main(args: Array<String>) {
 
     app.get("/playlist", { req, res ->
         res.setHeader("Content-Type", "application/x-mpegURL")
-        res.render("m3u.handlebars", Factory.audiobook())
+        res.render("m3u.handlebars", Factory.audiobook)
+    })
+
+    app.get("/playlist/chapters/:chapterID/stream.mp3", { req, res ->
+        val chapter = Factory.audiobook.chapters[req.params.chapterID]
+        val cipher = crypto.createCipheriv("aes128", chapter.key, chapter.iv);
+        request
+                .get(chapter.url)
+                .pipe(cipher)
+                .pipe(res)
+    })
+
+    app.get("/playlist/chapters/:chapterId/key", {req, res ->
+        console.log("DEBUG: Key for chapter: " + req.params.chapterId)
+        val chapter = Factory.audiobook.chapters[req.params.chapterId]
+        console.log("DEBUG: Chapter key: " + chapter.key)
+        res.setHeader("Content-Type", "binary/octet-stream")
+        res.send(chapter.key)
     })
 
     app.listen(3000, {
@@ -31,13 +52,20 @@ fun main(args: Array<String>) {
 }
 
 object Factory {
-    fun audiobook() : Audiobook{
-        val chap1 = Chapter(1, 290, "https://ia800504.us.archive.org/24/items/Item548HowToGoProWithAnEntryLevelDSLR/OMHT548.mp3" )
-        val chap2 = Chapter(2, 215, "https://ia800500.us.archive.org/32/items/Item335HowToMeditate/OMHT335.mp3")
-        return Audiobook(290 + 215, arrayOf(chap1, chap2))
+    val audiobook : Audiobook by lazy {
+
+        val chap1 = Chapter(1, 290,
+                crypto.randomBytes(16), crypto.randomBytes(16),
+                "https://ia800504.us.archive.org/24/items/Item548HowToGoProWithAnEntryLevelDSLR/OMHT548.mp3")
+        val chap2 = Chapter(2, 215,
+                crypto.randomBytes(16), crypto.randomBytes(16),
+                "https://ia800500.us.archive.org/32/items/Item335HowToMeditate/OMHT335.mp3")
+        return@lazy Audiobook(290 + 215, arrayOf(chap1, chap2))
     }
 }
 
 data class Audiobook(val duration: Long, val chapters: Array<Chapter>)
 
-data class Chapter(val index: Int, val duration: Long, val url: String)
+data class Chapter(val index: Int, val duration: Long, val key: ByteArray, val iv: ByteArray, val url: String) {
+    fun ivPresentation() : String = jsHelpers.toHexString(iv)
+}
